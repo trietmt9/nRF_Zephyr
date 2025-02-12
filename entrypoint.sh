0ecdd998d5d9c4ede249f062b28447ef406357de
trietmt9 <trietmt9@gmail.com>
2025-02-12 12:35:44 +0700

    update flash to board work flow

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /workdir/ncs/zephyr/zephyr-env.sh
-/bin/bash /workdir/zephyr-sdk-${ZEPHYR_TAG}/setup.sh -t arm-zephyr-eabi
-/bin/bash /workdir/zephyr-sdk-${ZEPHYR_TAG}/setup.sh -c
-
-exec $@
