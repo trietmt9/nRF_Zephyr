ab21ccfd8a33c0691ee6b17743098ea8f95fcbd7
trietmt9 <trietmt9@gmail.com>
2025-02-14 15:59:11 +0700

    adjust to UTF8

--- pythonLog.py
+++ pythonLog.py
@@ -6,7 +6,7 @@ ser = serial.Serial('/dev/ttyACM1', 115200)
 while True: 
     data = ser.readline()
     print(data)
-    f = open('log.txt', 'a').encoding('utf-8')
+    f = open('log.txt', 'a', encoding="utf-8")
     data = str(data)
     f.write(data + "\n")
     f.close() 
